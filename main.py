4a93f75b36dc255bf7c6e820f99894f4f6281680
Arman Mojaver <arman.mojaver@oneflow.com>
2022-04-26 17:25:26 +0100

    create cli and handler

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
+from time import sleep
+
+import click
 from binance.websocket.spot.websocket_client import SpotWebsocketClient as WebsocketClient
 
 
-def message_handler(message):
-    print(message)
+@click.command()
+@click.option('-s', '--symbol', type=str, default='bnbusdt', help='symbol')
+@click.option('-id', type=int, default=1, help='ID')
+@click.option('-l', '--limit', type=float, required=True)
+def main(symbol, id, limit):
+    def handler(result, limit=limit):
+        price = result.get('p')
+
+        diff = None
+        if price:
+            diff = float(price) >= limit
+
+        message = f"Price: {price}, Limit: {limit}, Target Reached: {diff}"
+        print(message)
+
+        sleep(0.5)
+
+    ws_client = WebsocketClient(stream_url='wss://stream.binance.com:9443')
+    ws_client.start()
+
+    ws_client.trade(symbol=symbol, id=id, callback=handler)
 
 
-ws_client = WebsocketClient(stream_url='wss://stream.binance.com:9443')
-ws_client.start()
-
-symbol = 'bnbusdt'
-id = 1
-callback = message_handler
-
-
-methods = {
-    'agg_trade': ws_client.agg_trade,
-    'trade': ws_client.trade,
-    'kline': ws_client.kline,
-}
-
-
-methods['agg_trade'](symbol=symbol, id=id, callback=callback)
+if __name__ == '__main__':
+    main()
