f07f8273a2c2c94c95dc3d9303f305af481304b8
Arman Mojaver <arman.mojaver@oneflow.com>
2022-04-26 12:42:35 +0100

    connect to the websocket

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from binance.websocket.spot.websocket_client import SpotWebsocketClient as WebsocketClient
 
+
+def message_handler(message):
+    print(message)
+
+
+ws_client = WebsocketClient(stream_url='wss://stream.binance.com:9443')
+ws_client.start()
+
+symbol = 'bnbusdt'
+id = 1
+callback = message_handler
+
+
+methods = {
+    'agg_trade': ws_client.agg_trade,
+    'trade': ws_client.trade,
+    'kline': ws_client.kline,
+}
+
+
+methods['agg_trade'](symbol=symbol, id=id, callback=callback)
